9ea1fb1269027c54ef194e6b76f127aac0fba12a
Oleg_T <oleg.terentev123456789@outlook.com>
2021-03-12 13:17:19 +0300

    Add necessary MySQL packages

--- GSTICK/Startup.cs
+++ GSTICK/Startup.cs
@@ -28,8 +28,7 @@ namespace GSTICK
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                options.UseMySQL(Configuration["ConnectionStrings:DefaultConnection"]));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
